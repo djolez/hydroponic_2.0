3d973e1fb6568b6802f30cdf29a15020872b1458
djolez <www.djolez@gmail.com>
2016-10-29 16:51:05 +0200

    Added output_simple.py and a mock relay to startup.py, other minor changes

--- esp8266/device.py
+++ esp8266/device.py
@@ -1,33 +1,39 @@
-import machine
 import logging
 
 import config
-
 import mqtt_client
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
 class Device:
+    types = { 'in': 1, 'out': 2 }
+
     def __init__(self, pin, device_type, name):
         self.pin = pin
         self.device_type = device_type
-        self.name = name
-        pin_type = machine.Pin.OUT if device_type is 'out' else machine.Pin.IN
-        self.pin_object = machine.Pin(pin, pin_type)
+        self.name = '{}_{}'.format(name, pin)
+        self.__setup_communication()
+        
+        logger.debug('{} -- Initialized'.format(self))
+
+    def __str__(self):
+        return '[name: {}]'.format(self.name)
 
     def __setup_communication(self):
-        self.mqtt_topic = '{}/device/{}'.format(config.CONFIG['client_id'], self.name)
-        self.mqtt_client = mqtt_client.make(self.mqtt_topic)
+        self.__mqtt_topic = '{}/device/{}'.format(config.CONFIG['client_id'], self.name)
+        self.__mqtt_client = mqtt_client.make(self.__mqtt_topic)
 
     def __send_msg(self, payload):
-        logger.debug('{} sending msg: '.format(self.name, payload))
-        self.mqtt_client.publish(self.mqtt_topic, payload)
-		
-    def change_state(self, state):
-        if(state is 'on'):
-            self.pin_object.value(0)
-        elif(state is 'off'):
-            self.pin_object.value(1)
+        logger.debug('{} -- Sending msg: {}'.format(self, payload))
+        self.__mqtt_client.publish(self.__mqtt_topic, payload)
 
-        logger.debug('Toggled device on pin: {}, state: {}'.format(self.pin, state))
+    def read(self):
+        raise NotImplementedError
+
+    def read_return(self, value):
+        self.__send_msg(b"{{'action': 'result', 'value': {} }}".format(value))
+
+    def write(self):
+        raise NotImplementedError
+
--- esp8266/startup.py
+++ esp8266/startup.py
@@ -1,12 +1,13 @@
-import sensor_implementation as sensors
-import device_implementation as devices
-import config
-import mqtt_client
-
 from time import sleep
 import logging
 import ujson
 
+import config
+import mqtt_client
+from ds18b20 import Ds18b20
+from output_simple import OutputSimple
+
+logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
 def on_message(topic, msg):
@@ -15,28 +16,33 @@ def on_message(topic, msg):
         data = ujson.loads(msg)
         if(data['action'] is 'reregister'):
             register()
-        elif(data['entity'] is 'sensor'):
-            sensors.handle_msg(data)
-        elif(data['entity'] is 'device'):
-            devices.handle_msg(data)
+        elif(data['action'] is 'read'):
+            devices[data['name']].read()
+        elif(data['action'] is 'write'):
+            devices[data['name']].write(data['value'])
     except ValueError:
-        print('Failed to parse msg: {}'.format(msg))
+        logger.error('Failed to parse msg: {}'.format(msg))
+    except KeyError as e:
+        logger.error('Message is missing field "{}"'.format(e))
 
 def register():
     data = {}
     data['action'] = 'module_on'
-    data['sensors'] = []
     data['devices'] = []
     data['module_name'] = config.CONFIG['client_id']
 
-    for name, sensor_object in sensors.sensors.items():
-        data['sensors'].append({'name': name})
-
-    for name, device_object in devices.devices.items():
+    for name, device_object in devices.items():
         data['devices'].append({'name': name, 'device_type': device_object.device_type})
 
     client.publish('main-dispatcher', ujson.dumps(data))
 
+def initialize_devices():
+    global devices
+
+    devices['ds18b20_10'] = Ds18b20(10)
+    devices['relay_3'] = OutputSimple(3)
+
+devices = {}
 client = None
 
 def main():
@@ -47,6 +53,7 @@ def main():
     client.set_callback(on_message)
     client.subscribe(config.CONFIG['client_id'])
 
+    initialize_devices()
     register()
 
     while True:
